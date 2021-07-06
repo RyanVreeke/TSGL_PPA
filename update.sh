bda3f4ae503981a64f9c608369015adcb6f0efbf
RyanVreeke <rvreeke0229@gmail.com>
2021-07-06 02:51:54 -0400

    Got rid of the libstb-dev dependency.

---
+++ update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+set -v
+
+export KEYNAME=894DA3E9EF8335101F9C041C479E75405454AF3F
+
+(
+    set -e
+    set -v
+
+    cd ./tsgl/
+
+    # Packages & Packages.gz
+    dpkg-scanpackages --multiversion . > Packages
+    gzip -k -f Packages
+
+    # Release, Release.gpg & InRelease
+    apt-ftparchive release . > Release
+    gpg --default-key "${KEYNAME}" -abs -o - Release > Release.gpg
+    gpg --default-key "${KEYNAME}" --clearsign -o - Release > InRelease
+)
+
